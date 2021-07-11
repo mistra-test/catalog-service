9e97e03180c2954b748edb31e5013114a03e2bce
Mistra <sperectrum@gmail.com>
2021-07-11 15:34:31 +0200

    switch back to non native

--- src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -1,29 +1,9 @@
 package com.example.catalogservice;
 
-import com.example.catalogservice.model.Movies;
-import com.example.catalogservice.model.Ratings;
-
-import org.apache.logging.log4j.message.ParameterizedMessageFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ConfigurationWarningsApplicationContextInitializer;
-import org.springframework.boot.context.ContextIdApplicationContextInitializer;
-import org.springframework.nativex.hint.AotProxyHint;
-import org.springframework.nativex.hint.ProxyBits;
-import org.springframework.nativex.hint.TypeHint;
 
 // @EnableJms
-@TypeHint(types = Ratings.class)
-@TypeHint(types = Movies.class)
-@TypeHint(types = ConfigurationWarningsApplicationContextInitializer.class)
-@TypeHint(types = ContextIdApplicationContextInitializer.class)
-@TypeHint(types = ParameterizedMessageFactory.class)
-@AotProxyHint(
-        targetClass = com.example.catalogservice.service.CatalogService.class,
-        proxyFeatures = ProxyBits.IS_STATIC)
-@AotProxyHint(
-        targetClass = com.example.catalogservice.controller.CatalogController.class,
-        proxyFeatures = ProxyBits.IS_STATIC)
 @SpringBootApplication
 public class CatalogServiceApplication {
 
